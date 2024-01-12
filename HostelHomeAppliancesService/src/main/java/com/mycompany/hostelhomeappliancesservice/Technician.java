09ff9cc205c2eb4c383383ef5cc5a7223b8e0834
JerShin <tp067844@mail.apu.edu.my>
2024-01-12 10:59:49 +0800

    Add Technician class with properties and methods

---
+++ HostelHomeAppliancesService/src/main/java/com/mycompany/hostelhomeappliancesservice/Technician.java
@@ -0,0 +1,59 @@
+package com.mycompany.hostelhomeappliancesservice;
+
+public class Technician extends User {
+    private int technicianID;
+    private String specialization;
+
+    public Technician(int technicianID, int userID, String name, String email, String password, String specialization) {
+        super(userID, name, email, password);
+        this.technicianID = technicianID;
+        this.specialization = specialization;
+    }
+
+    public Technician(int userID, String name, String email, String password, int technicianID, String specialization) {
+        super(userID, name, email, password);
+        this.technicianID = technicianID;
+        this.specialization = specialization;
+    }
+
+    public Technician(int userID, String name, String email, String password, String specialization) {
+        super(userID, name, email, password);
+        this.specialization = specialization;
+    }
+
+    public Technician(String name, String email, String password, String specialization) {
+        super(name, email, password);
+        this.specialization = specialization;
+    }
+
+    public int getTechnicianID() {
+        return technicianID;
+    }
+
+    public void setTechnicianID(int technicianID) {
+        if (technicianID > 0) {
+            this.technicianID = technicianID;
+        }
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        if (specialization != null) {
+            this.specialization = specialization;
+        }
+    }
+
+    public void getTechnicianDetails() {
+        System.out.println("Technician ID: " + technicianID);
+        System.out.println("User ID: " + userID);
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+    }
+
+    public String toString() {
+        return "Technician ID: " + technicianID + "\nUser ID: " + userID + "\nName: " + name + "\nEmail: " + email;
+    }
+}
