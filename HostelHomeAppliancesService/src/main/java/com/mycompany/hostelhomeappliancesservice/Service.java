1534b11fd0d92f5d47f9d3e28b7b9b93b35d9746
JerShin21 <tp067844@mail.apu.edu.my>
2024-01-12 13:29:39 +0800

    Added service class

---
+++ HostelHomeAppliancesService/src/main/java/com/mycompany/hostelhomeappliancesservice/Service.java
@@ -0,0 +1,9 @@
+package com.mycompany.hostelhomeappliancesservice;
+
+public class Service {
+    private int serviceID;
+    private String serviceType;
+    private String serviceDescription;
+    private int serviceCost;
+    private int serviceDuration;
+}
