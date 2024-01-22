b74a8432a8f456decf47c25c331d1cc54cfa3ce3
JerShin21 <tp067844@mail.apu.edu.my>
2024-01-22 16:13:33 +0800

    Delete unnecessary classes

--- HostelHomeAppliancesService/src/main/java/com/mycompany/hostelhomeappliancesservice/Service.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.hostelhomeappliancesservice;
-
-public class Service {
-    private int serviceID;
-    private String serviceType;
-    private String serviceDescription;
-    private int serviceCost;
-    private int serviceDuration;
-}
